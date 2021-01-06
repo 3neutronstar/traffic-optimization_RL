5c4d60bb26dbf6af08cbd6e220fdd6bc88a448c6
3neutronstar <minsoo.kang0918@gmail.com>
2021-01-06 22:25:50 +0900

    function of making the location of node when the node id is given is set

--- 3x3grid_make.py
+++ 3x3grid_make.py
@@ -1,21 +1,72 @@
-node_id=["n_nl","n_nr",
-"n_ln","n_cnl","n_cnr","n_rn",
-"n_ls","n_csl","n_csr","n_rs",
-"n_sl" ,"n_sr" ]
+import xml.etree.cElementTree as ET
+node_id=["n_nl","n_nc","n_nr",
+ "n_ln","n_cnl","n_cnc","n_cnr","n_rn",
+ "n_lc","n_ccl","n_ccc","n_ccr","n_rc",
+ "n_ls","n_csl","n_csc","n_csr","n_rs",
+        "n_sl" ,"n_sc","n_sr" ]
 
+laneLength=100.0
+node_dict=dict()
+def specify_node(node_id):
+    for node in node_id:
+        node_dict['node id']=node
+        #y value decision
+        if node[2]=='n':
+            y=2.0*laneLength
+        elif node[2]=='s':
+            y=-2.0*laneLength
+        else: # node[2]=='c'
+            node_dict['type']='traffic_light' #n_c 계열은 모두 신호등
+            node_dict['tl']=node
+            if node[3]=='n':
+                y=laneLength
+            elif node[3]=='s':
+                y=(-1.0)*laneLength
+            else: # node[3]=='c'
+                y=0
+        #x value decision
+        if node[2]=='l':
+            x=-2.0*laneLength
+        elif node[2]=='r':
+            x=2.0*laneLength
+        else: # node[2]='c'
+            try:
+                if node[-1]=='l':
+                    x=(-1.0)*laneLength
+                elif node[-1]=='r':
+                    x=laneLength
+                else: # node[-1]=='c'
+                    x=0
+            except ValueError or IndexError as e:
+                raise NotImplementedError from e
+        node_dict['x']=x
+        node_dict['y']=y
+        print(node_dict)
+        
+specify_node(node_id)
+            
 edge_dict={
     "n_nl":["n_cnl"],
-    "n_cnl":["n_nl","n_ln","n_cnr","n_csl"],
+    "n_nc":["n_cnc"],
     "n_nr":["n_cnr"],
-    "n_cnr":["n_nr","n_cnl","n_rn","n_csr"],
     "n_ln":["n_cnl"],
-    "n_rn":["n_cnr"],
-    "n_csl":["n_cnl","n_ls","n_csr","n_sl"],
-    "n_csr":["n_cnr","n_csl","n_rs","n_sr"],
+    "n_lc":["n_ccl"],
     "n_ls":["n_csl"],
+    "n_rn":["n_cnr"],
+    "n_rc":["n_ccr"],
     "n_rs":["n_csr"],
     "n_sl":["n_csl"],
-    "n_sr":["n_csr"]
+    "n_sc":["n_csc"],
+    "n_sr":["n_csr"],
+    "n_cnl":["n_nl","n_ln","n_cnr","n_csl"],
+    "n_cnc":["n_nc","n_ccc","n_cnl","n_cnr"],
+    "n_cnr":["n_nr","n_cnl","n_rn","n_csr"],
+    "n_csl":["n_ccl","n_ls","n_csc","n_sl"],
+    "n_csc":["n_csl","n_ccc","n_sc","n_csr"],
+    "n_csr":["n_csc","n_ccr","n_rs","n_sr"],
+    "n_ccl":["n_lc","n_csl","n_cnl","n_ccc"],
+    "n_ccc":["n_ccl","n_cnc","n_ccr","n_csc"],
+    "n_ccr":["n_ccc","n_cnr","n_rc","n_csr"],
 }
 
 def specify_edgeid(edge_dict):
