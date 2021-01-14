f848ea9d26bcd316179eccc1d883f64e52f28763
3neutronstar <3neutronstar@naver.com>
2021-01-14 23:48:38 +0900

    add grid3x3 xml parsing and output file generation

--- grid_3x3flow.py
+++ grid_3x3flow.py
@@ -1,19 +1,6 @@
-import xml.etree.cElementTree as ET
-from xml.etree.ElementTree import dump
-from lxml import etree as ET
-import os
-E = ET.Element
+from gen_net import Network
+from gen_net import configs
 
-configs = {
-    'num_lanes': 3,
-    'file_name': '3x3intersection',
-    'laneLength': 100.0,
-    'num_cars': 1800,
-    'flow_start': 0,
-    'flow_end': 9000,
-    'sim_start': 0,
-    'sim_end': 10000,
-}
 node_id = ["n_nl", "n_nc", "n_nr",
            "n_ln", "n_cnl", "n_cnc", "n_cnr", "n_rn",
            "n_lc", "n_ccl", "n_ccc", "n_ccr", "n_rc",
@@ -45,42 +32,11 @@ edge_dict = {
 }
 
 
-def indent(elem, level=0):
-    i = "\n\t" + level*""
-    if len(elem):
-        if not elem.text or not elem.text.strip():
-            elem.text = i + ""
-            if not elem.tail or not elem.tail.strip():
-                elem.tail = i
-            for elem in elem:
-                indent(elem, level+1)
-            if not elem.tail or not elem.tail.strip():
-                elem.tail = i
-        else:
-            if level and (not elem.tail or not elem.tail.strip()):
-                elem.tail = i
-
-
-class Network():
+class Grid3x3network(Network):
     def __init__(self, node_id, edge_dict, configs):
-        self.sim_start = configs['sim_start']
-        self.sim_end = configs['sim_end']
+        super().__init__(configs)
         self.node_id = node_id
         self.edge_dict = edge_dict
-        self.file_name = configs['file_name']
-        self.xml_node_name = configs['file_name']+'.nod'
-        self.xml_edg_name = configs['file_name']+'.edg'
-        self.xml_route_pos = self.file_name+'.rou'
-        self.current_path = './'
-        self.num_cars = str(configs['num_cars'])
-        self.num_lanes = str(configs['num_lanes'])
-        self.flow_start = str(configs['flow_start'])
-        self.flow_end = str(configs['flow_end'])
-        self.laneLength = configs['laneLength']
-        print(configs)
-        self.node = self.specify_node()  # node info
-        self.flows = list()
-        self.vehicles = list()
 
     def specify_edge(self):
         edges = list()
@@ -138,42 +94,6 @@ class Network():
         self.node = node_list
         return node_list
 
-    def generate_nod_xml(self):
-        self.specify_node()
-        self.xml_nod_pos = os.path.join(self.current_path, self.xml_node_name)
-        nod_xml = ET.Element('nodes')
-
-        for node_dict in self.node:
-            # node_dict['x']=format(node_dict['x'],'.1f')
-            nod_xml.append(E('node', attrib=node_dict))
-            indent(nod_xml)
-        '''
-        for node_attributes in nodes:
-            x.append(E('node', **node_attributes))
-        '''
-        dump(nod_xml)
-        tree = ET.ElementTree(nod_xml)
-        # tree.write(self.xml_nod_pos+'.xml',encoding='utf-8',xml_declaration=True)
-        tree.write(self.xml_nod_pos+'.xml', pretty_print=True,
-                   encoding='UTF-8', xml_declaration=True)
-
-    def generate_edg_xml(self):
-        self.specify_edge()
-        self.xml_edg_pos = os.path.join(self.current_path, self.xml_edg_name)
-        edg_xml = ET.Element('edges')
-        for _, edge_dict in enumerate(self.edges):
-            edg_xml.append(E('edge', attrib=edge_dict))
-            indent(edg_xml)
-        dump(edg_xml)
-        tree = ET.ElementTree(edg_xml)
-        # tree.write(self.xml_edg_pos+'.xml',encoding='utf-8',xml_declaration=True)
-        tree.write(self.xml_edg_pos+'.xml', pretty_print=True,
-                   encoding='UTF-8', xml_declaration=True)
-
-    def generate_net_xml(self):
-        os.system('netconvert --node-files={}.nod.xml --edge-files={}.edg.xml --output-file={}.net.xml'.format(
-            self.file_name, self.file_name, self.file_name))
-
     def specify_flow(self):
         edge_start = list()
         node_fin = list()
@@ -220,63 +140,6 @@ class Network():
 
         return self.flows
 
-    def generate_rou_xml(self):
-        self.specify_flow()
-        route_xml = ET.Element('routes')
-        if len(self.vehicles) != 0:  # empty
-            for _, vehicle_dict in enumerate(self.vehicles):
-                route_xml.append(E('veh', attrib=vehicle_dict))
-                indent(route_xml)
-        if len(self.flows) != 0:
-            for _, flow_dict in enumerate(self.flows):
-                route_xml.append(E('flow', attrib=flow_dict))
-                indent(route_xml)
-        dump(route_xml)
-        tree = ET.ElementTree(route_xml)
-        tree.write(self.xml_route_pos+'.xml', pretty_print=True,
-                   encoding='UTF-8', xml_declaration=True)
 
-    def generate_cfg(self):
-        # attrib={'xmlns:xmi':'http://www.w3.org/2001/XMLSchema-instance','xsi:noNamespaceSchemaLocation':'http://sumo.sf.net/xsd/netconvertConfiguration.xsd'}
-        sumocfg = ET.Element('configuration')
-        inputXML = ET.SubElement(sumocfg, 'input')
-        inputXML.append(
-            E('net-file', attrib={'value': self.file_name+'.net.xml'}))
-        indent(sumocfg)
-        if os.path.exists(os.path.join(self.current_path, self.file_name+'.rou.xml')):
-            inputXML.append(
-                E('route-files', attrib={'value': self.file_name+'.rou.xml'}))
-            indent(sumocfg)
-
-        if os.path.exists(os.path.join(self.current_path, self.file_name+'.add.xml')):
-            inputXML.append(
-                E('add-file', attrib={'value': self.file_name+'.add.xml'}))
-            indent(sumocfg)
-
-        time = ET.SubElement(sumocfg, 'time')
-        time.append(E('begin', attrib={'value': str(self.sim_start)}))
-        indent(sumocfg)
-        time.append(E('end', attrib={'value': str(self.sim_end)}))
-        indent(sumocfg)
-        outputXML = ET.SubElement(sumocfg, 'output')
-        outputXML.append(
-            E('netstate-dump', attrib={'value': self.file_name+'_dump.net.xml'}))
-        indent(sumocfg)
-        dump(sumocfg)
-        tree = ET.ElementTree(sumocfg)
-        tree.write(self.file_name+'_simulate'+'.sumocfg',
-                   pretty_print=True, encoding='UTF-8', xml_declaration=True)
-
-    def sumogui(self):
-        os.system('sumo-gui -c {}.sumocfg'.format(self.current_path +
-                                                  self.file_name+'_simulate'))
-
-
-if __name__ == '__main__':
-    network = Network(node_id, edge_dict, configs)
-    network.generate_edg_xml()
-    network.generate_nod_xml()
-    network.generate_net_xml()
-    network.generate_rou_xml()
-    network.generate_cfg()
-    network.sumogui()
+a = Grid3x3network(node_id, edge_dict, configs)
+a.sumo_gui()
