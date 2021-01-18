a282b0633cbf724cf7a8eb4fb13cd7dcaea00fda
3neutronstar <3neutronstar@naver.com>
2021-01-18 22:53:13 +0900

    dqn made it

--- Agent/dqn.py
+++ Agent/dqn.py
@@ -3,14 +3,15 @@ import torch
 from torch import nn
 import torch.nn.functional as f
 import numpy as np
+import torch.autograd
 import torch.optim as optim
 import random
 
-from collections import deque,namedtuple
+from collections import deque, namedtuple
 from copy import deepcopy
 
 Transition = namedtuple('Transition',
-                        ('state', 'action', 'next_state', 'reward'))
+                        ('state', 'action', 'reward', 'next_state'))
 
 
 class ReplayMemory(object):
@@ -33,8 +34,9 @@ class ReplayMemory(object):
     def __len__(self):
         return len(self.memory)
 
+
 class QNetwork(nn.Module):
-    def __init__(self, input_size,output_size,configs):
+    def __init__(self, input_size, output_size, configs):
         super(QNetwork, self).__init__()
         self.configs = configs
         self.input_size = input_size
@@ -43,9 +45,9 @@ class QNetwork(nn.Module):
 
         # build nn
         self.fc = list()
-        self.fc1=nn.Linear(self.input_size,40)
-        self.fc2=nn.Linear(40,30)
-        self.fc3=nn.Linear(30,self.output_size)
+        self.fc1 = nn.Linear(self.input_size, 40)
+        self.fc2 = nn.Linear(40, 30)
+        self.fc3 = nn.Linear(30, self.output_size)
 
     def forward(self, state):
         x = state
@@ -59,35 +61,35 @@ class Trainer(RLAlgorithm):
         super().__init__(configs)
         configs['batch_size'] = 32
         configs['experience_replay_size'] = 1e5
-        configs['input_size']=16
-        configs['output_size']=8
-        configs['action_space']=8
+        configs['input_size'] = 16
+        configs['output_size'] = 8
+        configs['action_space'] = 8
         self.configs = configs
         self.input_size = self.configs['input_size']
         self.output_size = self.configs['output_size']
         self.action_space = self.configs['action_space']
-        self.gamma=self.configs['gamma']
+        self.gamma = self.configs['gamma']
         self.epsilon = 0.5
         self.criterion = nn.MSELoss()
-        self.experience_replay = ReplayMemory(self.configs['experience_replay_size'])
+        self.experience_replay = ReplayMemory(
+            self.configs['experience_replay_size'])
         self.batch_size = self.configs['batch_size']
 
-        if self.configs['model']=='FRAP':
+        if self.configs['model'] == 'FRAP':
             from Agent.Model.FRAP import FRAP
-            model=FRAP(self.input_size,self.output_size)
-            model.add_module(QNetwork(self.input_size,self.output_size,self.configs))
-            print(model)
+            model = FRAP(self.input_size, self.output_size)
+            model.add_module(
+                QNetwork(self.input_size, self.output_size, self.configs))
         else:
-            model=QNetwork(16,8,configs)# 1개 네트워크용
+            model = QNetwork(16, 8, configs)  # 1개 네트워크용
         model.to(configs['device'])
         print(model)
-        self.mainQNetwork = deepcopy(model)
-        self.targetQNetwork = deepcopy(model)
+        self.mainQNetwork = deepcopy(model).to(self.configs['device'])
+        self.targetQNetwork = deepcopy(model).to(self.configs['device'])
         self.optimizer = optim.Adam(
             self.mainQNetwork.parameters(), lr=configs['learning_rate'])
         self.configs['experience_replay_size'] = 20000
-        for param in self.targetQNetwork.parameters():  # target Q는 hold
-            param.requires_grad = False
+        self.targetQNetwork.eval()
         self.mainQNetwork.train()  # train모드로 설정
 
         self.running_loss = 0
@@ -97,15 +99,15 @@ class Trainer(RLAlgorithm):
             self.mainQNetwork.eval()
 
     def get_action(self, state):
-        sample=random.random()
+        sample = random.random()
         with torch.no_grad():
-            self.Q=self.mainQNetwork(state)
-        _,action=torch.max(self.Q,dim=1)
-        print(action)
+            self.Q = self.mainQNetwork(state)
+        _, action = torch.max(self.Q, dim=0)  # 가로로
         if sample < self.epsilon:
-                return action
+            return action
         else:
-            action=torch.tensor([random.randint(0,1) for i in range(8)],device=self.configs['device'])
+            action = torch.tensor([random.randint(0, 1)
+                                   for i in range(8)], device=self.configs['device'])
             return action
 
     def get_loss(self):
@@ -115,45 +117,57 @@ class Trainer(RLAlgorithm):
         self.targetQNetwork.load_state_dict(self.mainQNetwork.state_dict())
 
     def save_replay(self, state, action, reward, next_state):
-        self.experience_replay.push(state,action,reward,next_state)
+        self.experience_replay.push(
+            state.reshape(1, -1), action, reward, next_state.reshape(1, -1))
 
     def update(self, done=False):
-        if len(self.experience_replay)<self.configs['batch_size']:
+        if len(self.experience_replay) < self.configs['batch_size']:
             return
-        
+
         transitions = self.experience_replay.sample(self.configs['batch_size'])
         # Transpose the batch (자세한 설명을 위해 http://stackoverflow.com/a/19343/3343043 를 보십시오).
         batch = Transition(*zip(*transitions))
 
         # 최종 상태가 아닌 마스크를 계산하고 배치 요소를 연결합니다.
         non_final_mask = torch.tensor(tuple(map(lambda s: s is not None,
-                                            batch.next_state)), device=self.configs['device'], dtype=torch.uint8)
-        non_final_next_states = batch.next_state# torch.cat([s for s in torch.tensor(batch.next_state)
-                                #                    if s is not None])
-        
-        state_batch = torch.cat(batch.state)
-        action_batch = torch.cat(batch.action)
-        reward_batch = torch.cat(batch.reward)
-        
+                                                batch.next_state)), device=self.configs['device'], dtype=torch.long)
+        non_final_mask.reshape(-1, 1)
+        # non_final_next_states = torch.tensor([s for s in batch.next_state
+        #                                       if s is not None]).reshape(-1, 1)
+        non_final_next_states = torch.cat([s for s in batch.next_state
+                                           if s is not None], dim=0).float()
+        state_batch = torch.cat(batch.state, dim=0)
+        action_batch = torch.cat(batch.action, dim=0)
+
+        # reward_batch = torch.cat(torch.tensor(batch.reward, dim=0)
+        reward_batch = torch.tensor(batch.reward).reshape(32)
 
         # Q(s_t, a) 계산 - 모델이 Q(s_t)를 계산하고, 취한 행동의 칼럼을 선택한다.
 
-        state_action_values = self.mainQNetwork(state_batch).gather(1,action_batch)
+        # state_action_values = self.mainQNetwork(
+        #     state_batch).gather(1, action_batch)  # for 3D
+        state_action_values = self.mainQNetwork(
+            state_batch).max(1)[0].clone().float()
+        state_action_values.requires_grad = True
 
         # 모든 다음 상태를 위한 V(s_{t+1}) 계산
-        next_state_values = torch.zeros(self.configs['batch_size'], device=self.configs['device'])
-        next_state_values[non_final_mask] = self.targetQNetwork(non_final_next_states).max(1)[0].detach()
+        next_state_values = torch.zeros(
+            self.configs['batch_size'], device=self.configs['device'], dtype=torch.float)
+        next_state_values[non_final_mask] = self.targetQNetwork(
+            non_final_next_states).max(1)[0].detach()
         # 기대 Q 값 계산
-        expected_state_action_values = (next_state_values * self.configs['gamma']) + reward_batch
+        expected_state_action_values = (
+            next_state_values * self.configs['gamma']) + reward_batch
 
         # loss 계산
-        loss = self.criterion(state_action_values, expected_state_action_values.unsqueeze(1))
+        loss = self.criterion(state_action_values,
+                              expected_state_action_values.unsqueeze(1))
 
         # 모델 최적화
         self.optimizer.zero_grad()
         loss.backward()
-        for param in self.mainQNetwork.parameters():
-            param.grad.data.clamp_(-1, 1)
+        # for param in self.mainQNetwork.parameters():
+        #     param.grad.data.clamp_(-1, 1)
         self.optimizer.step()
 
         if self.epsilon > 0.2:
--- Env/env.py
+++ Env/env.py
@@ -4,44 +4,44 @@ import traci
 from Env.base import baseEnv
 from copy import deepcopy
 
+
 class TLEnv(baseEnv):
     def __init__(self, tl_rlList, configs):
         self.configs = configs
         self.tl_rlList = tl_rlList
         self.tl_list = traci.trafficlight.getIDList()
         self.edge_list = traci.edge.getIDList()
-        self.pressure=0
+        self.pressure = 0
         '''
         up right down left 순서대로 저장
 
         '''
         # pressure_dict=dict()
         # for i,edge in enumerate(self.edge_list):
-        #     if edge[4]=='u': # 
+        #     if edge[4]=='u': #
         #         string=
-        self.interest_list=[
+        self.interest_list = [
             {
-                'id':'u_0_0',
-                'inflow':'n_0_u_to_n_0_0',
+                'id': 'u_0_0',
+                'inflow': 'n_0_u_to_n_0_0',
                 'outflow': 'n_0_0_to_n_0_d',
             },
             {
-                'id':'r_0_0',
-                'inflow':'n_0_r_to_n_0_0',
+                'id': 'r_0_0',
+                'inflow': 'n_0_r_to_n_0_0',
                 'outflow': 'n_0_0_to_n_0_l',
             },
             {
-                'id':'d_0_0',
-                'inflow':'n_0_d_to_n_0_0',
+                'id': 'd_0_0',
+                'inflow': 'n_0_d_to_n_0_0',
                 'outflow': 'n_0_0_to_n_0_u',
             },
             {
-                'id':'l_0_0',
-                'inflow':'n_0_l_to_n_0_0',
+                'id': 'l_0_0',
+                'inflow': 'n_0_l_to_n_0_0',
                 'outflow': 'n_0_0_to_n_0_r',
             }
         ]
-
 
         # for _, edges in enumerate(self.edge_list):
         #     for j, rl_node in enumerate(self.tl_rlList):
@@ -49,13 +49,15 @@ class TLEnv(baseEnv):
         #             self.interest_outEdge.append(edges)
         #         if edges[:5]==rl_node: # inflow
         #             self.interest_inEdge.append(edges)
-        self.phase_size = len(traci.trafficlight.getRedYellowGreenState(self.tl_list[0]))
-
+        self.phase_size = len(
+            traci.trafficlight.getRedYellowGreenState(self.tl_list[0]))
 
     def get_state(self):
         phase = list()
-        state = torch.zeros((2,8),device=self.configs['device'],dtype=torch.int) #기준
-        vehicle_state = torch.zeros((8,1),device=self.configs['device'],dtype=torch.int)
+        state = torch.zeros(
+            (2, 8), device=self.configs['device'], dtype=torch.int)  # 기준
+        vehicle_state = torch.zeros(
+            (8, 1), device=self.configs['device'], dtype=torch.int)
         # for _, edge in enumerate(edge_list): # 이 부분을 밖에서 list로 구성해오면 쉬움
         #     if edge[-5:]=='n_2_2': # outflow 여기에 n_2_2대신에 tl_id를 넣으면 pressure가 되는 것
         #         inflow+=traci.edge.getLastStepVehicleNumber(edge)
@@ -67,24 +69,27 @@ class TLEnv(baseEnv):
             phase.append(traci.trafficlight.getRedYellowGreenState(tl_rl))
 
         # 1교차로용 n교차로는 추가요망
-        phase_state=self._toState(phase[0])
-        for i,interest in enumerate(self.interest_list):
-            vehicle_state[2*i+1]= traci.edge.getLastStepVehicleNumber(interest['inflow'])# 죄회전용 추가 필요
-        vehicle_state=torch.transpose(vehicle_state,0,1)
-        state=torch.cat((vehicle_state,phase_state),dim=1)
-        print(state.size())
+        phase_state = self._toState(phase[0])
+        for i, interest in enumerate(self.interest_list):
+            # 죄회전용 추가 필요
+            vehicle_state[2*i +
+                          1] = traci.edge.getLastStepVehicleNumber(interest['inflow'])
+        vehicle_state = torch.transpose(vehicle_state, 0, 1)
+        state = torch.cat((vehicle_state, phase_state), dim=0)
         return state
 
     def collect_state(self):
         '''
         갱신 및 점수 확정용 함수
         '''
-        inflow_rate=0
-        outflow_rate=0
-        for _,interest in enumerate(self.interest_list):
-            inflow_rate += traci.edge.getLastStepVehicleNumber(interest['inflow'])
-            outflow_rate += traci.edge.getLastStepVehicleNumber(interest['outflow'])
-        self.pressure+=outflow_rate-inflow_rate
+        inflow_rate = 0
+        outflow_rate = 0
+        for _, interest in enumerate(self.interest_list):
+            inflow_rate += traci.edge.getLastStepVehicleNumber(
+                interest['inflow'])
+            outflow_rate += traci.edge.getLastStepVehicleNumber(
+                interest['outflow'])
+        self.pressure += (outflow_rate-inflow_rate)
 
     def step(self, action):
         '''
@@ -92,11 +97,11 @@ class TLEnv(baseEnv):
         '''
         phase = self._toPhase(action)  # action을 분해
 
-        #action을 environment에 등록 후 상황 살피기
+        # action을 environment에 등록 후 상황 살피기
         for _, tl_rl in enumerate(self.tl_rlList):
             traci.trafficlight.setRedYellowGreenState(tl_rl, phase)
-        
-        # reward calculation and save 
+
+        # reward calculation and save
 
     def get_reward(self):
         '''
@@ -104,8 +109,8 @@ class TLEnv(baseEnv):
         Max Pressure based control
         각 node에 대해서 inflow 차량 수와 outflow 차량수 + 해당 방향이라는 전제에서
         '''
-        reward=deepcopy(self.pressure)
-        self.pressure=0
+        reward = deepcopy(self.pressure)
+        self.pressure = 0
         return reward
 
     def _toPhase(self, action):  # action을 해석가능한 phase로 변환
@@ -121,11 +126,11 @@ class TLEnv(baseEnv):
         for i in range(4):  # 4차로
             phase = phase + 'g'+self.configs['num_lanes']*signal[2*i] + \
                 signal[2*i+1]+'r'  # 마지막 r은 u-turn
-        print(phase)
         return phase
 
     def _toState(self, phase):  # env의 phase를 해석불가능한 state로 변환
-        state = torch.zeros((8,1),device=self.configs['device'], dtype=torch.int16)
+        state = torch.zeros(
+            (8, 1), device=self.configs['device'], dtype=torch.int16)
         for i in range(4):  # 4차로
             phase = phase[:][1:]  # 우회전
             state[i] = self._mappingMovement(phase[0])  # 직진신호 추출
@@ -133,7 +138,7 @@ class TLEnv(baseEnv):
             state[i+1] = self._mappingMovement(phase[0])  # 좌회전신호 추출
             phase = phase[1:]  # 좌회전
             phase = phase[1:]  # 유턴
-        state=torch.transpose(state,0,1)
+        state = torch.transpose(state, 0, 1)
         return state
 
     def _getMovement(self, num):
@@ -151,5 +156,3 @@ class TLEnv(baseEnv):
             return 0
         else:
             return -1  # error
-    
-
