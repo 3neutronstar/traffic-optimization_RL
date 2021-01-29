e232fea0783e7c94ef6499d47fcb7efcf38740f5
3neutronstar <3neutronstar@naver.com>
2021-01-29 21:34:16 +0900

    add gen_tllogic

--- util/gen_tllogic.py
+++ util/gen_tllogic.py
@@ -1,11 +1,14 @@
-import xml.etree.cElementTree as ET
+#import xml.etree.cElementTree as ET
 from xml.etree.ElementTree import dump
 from lxml import etree as ET
+import lxml
 import os
 import argparse
 import sys
 from xml.etree.ElementTree import parse
 E = ET.Element
+
+
 def parse_args(args):
     parser = argparse.ArgumentParser(
         formatter_class=argparse.RawDescriptionHelpFormatter,
@@ -14,40 +17,67 @@ def parse_args(args):
 
     # optional input parameters
     parser.add_argument(
-        '--file_path',type=str,default=None,
+        '--file_path', type=str, default=None,
         help='comparision group file name')
     parser.add_argument(
-        '--file',type=str,default=None,
+        '--file', type=str, default=None,
         help='comparision group file name')
     return parser.parse_known_args(args)[0]
 
+
 def tl_logic(file_path):
     # file load
-    tree=parse(file_path)
+    tree = parse(file_path)
     # file write setup
+    tl_additional_default = ET.Element('additional')
     tl_additional = ET.Element('additional')
 
     # find and write
-    trafficSignalList=tree.findall('trafficSignal')
+    trafficSignalList = tree.findall('trafficSignal')
+    # trafficSignal내에서 찾음
     for trafficSignal in trafficSignalList:
-        TODPlan=trafficSignal.findall('TODPlan')
-        scheduleList=trafficSignal.findall('schedule')
-        
+        TODPlan = trafficSignal.findall('TODPlan')
+        scheduleList = trafficSignal.findall('schedule')
+        # 찾고싶은 id를 여기서 start time을 적고 넣으시오
+        for plan in TODPlan[0].findall('plan'):
+            if plan.attrib['startTime'] == '25200':  # 9시
+                id = plan.attrib['schedule']
+
+        # schedule들 내에서 찾는 과정
         for schedule in scheduleList:
-            if TODPlan[0].attrib['defaultPlan'] == schedule.attrib['id']:
-                tlLogic = ET.SubElement(tl_additional, 'tlLogic', attrib=schedule)
-                phase_set=schedule.findall('phase')
-                for phase in phase_set
-                    phase_xml=ET.SubElement(tlLogic,attrib=)
-        print("end")
+            # node id 교체
+            if TODPlan[0].attrib['defaultPlan'] == schedule.attrib['id']:  # default
+                schedule.attrib['id'] = trafficSignal.attrib['nodeID']
+                tlLogic_default = ET.SubElement(
+                    tl_additional_default, 'tlLogic', attrib=schedule)
+                phase_set = schedule.findall('phase')
+                for phase in phase_set:
+                    phase_dict = dict()
+                    for key in phase.keys():
+                        phase_dict[key] = phase.attrib[key]
+                    tlLogic_default.append(E('phase', attrib=phase_dict))
 
+            if id == schedule.attrib['id']:
+                schedule.attrib['id'] = trafficSignal.attrib['nodeID']
+                tlLogic = ET.SubElement(
+                    tl_additional, 'tlLogic', attrib=schedule)
+                phase_set = schedule.findall('phase')
+                for phase in phase_set:
+                    phase_dict = dict()
+                    for key in phase.keys():
+                        phase_dict[key] = phase.attrib[key]
+                    tlLogic.append(E('phase', attrib=phase_dict))
 
-    dump(tl_additional)
+    print("end")
+
+    dump(tl_additional_default)
+    writetree = ET.ElementTree(tl_additional_default)
+    writetree.write(os.path.join('./output_default_tl.add.xml'),
+                    pretty_print=True, encoding='UTF-8', xml_declaration=True)
     writetree = ET.ElementTree(tl_additional)
-    writetree.write(os.path.join('./util/output_tl.add.xml'),
-                pretty_print=True, encoding='UTF-8', xml_declaration=True)
+    writetree.write(os.path.join('./output_tl.add.xml'),
+                    pretty_print=True, encoding='UTF-8', xml_declaration=True)
     print(scheduleList)
-    
 
     # if len(self.traffic_light) != 0 or .configs['mode'] == 'simulate':
     #     for _, tl in enumerate(traffic_light_set):
@@ -64,17 +94,16 @@ def tl_logic(file_path):
     #             pretty_print=True, encoding='UTF-8', xml_declaration=True)
 
 
-
 def main(args):
-    flags=parse_args(args)
-    if flags.file!=None:
-        path=os.path.join(os.path.dirname(os.path.abspath(__file__)),flags.file+'.xml')
+    flags = parse_args(args)
+    if flags.file != None:
+        path = os.path.join(os.path.dirname(
+            os.path.abspath(__file__)), flags.file+'.xml')
     else:
-        path= os.path.join(flags.file_path,'.xml')
+        path = os.path.join(flags.file_path, '.xml')
 
     tl_logic(path)
-    
 
 
-if __name__ =='__main__':
+if __name__ == '__main__':
     main(sys.argv[1:])
