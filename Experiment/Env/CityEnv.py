c5b98edc78e55f7267e1951ec10c4563446b8d98
3neutronstar <3neutronstar@naver.com>
2021-04-02 21:03:59 +0900

    change the value for update mask

--- Experiment/Env/CityEnv.py
+++ Experiment/Env/CityEnv.py
@@ -112,13 +112,14 @@ class CityEnv(baseEnv):
                 action_change_mask[index] = True
 
         # Reward
+        # print("action", action_change_mask)
         for index in torch.nonzero(action_change_mask):
             outflow = 0
             inflow = 0
             interests = self.node_interest_pair[self.tl_rl_list[index]]
             for interest in interests:
                 if interest['outflow']:  # None이 아닐 때 행동
-                    outflow += (traci.edge.getLastStepVehicleNumber(
+                    outflow += (traci.edge.getLastStepHaltingNumber(
                         interest['outflow']))/100.0
                 if interest['inflow']:  # None이 아닐 때 행동
                     inflow += traci.edge.getLastStepVehicleNumber(
@@ -148,8 +149,8 @@ class CityEnv(baseEnv):
         # action 변화를 위한 state
         next_states = torch.zeros(
             (1, self.state_space, 4, self.num_agent), dtype=torch.float, device=self.device)
-
-        for idx in torch.nonzero(action_update_mask):
+        # print(action_update_mask)
+        for idx in torch.nonzero(action_change_mask):
             next_state = list()
             # 모든 rl node에 대해서
             phase_type_tensor = torch.tensor(self.configs['phase_type'][idx])
@@ -169,24 +170,23 @@ class CityEnv(baseEnv):
                         pair['inflow'])/100.0-left_movement  # 가장 좌측에 멈춘 친구를 왼쪽차선 이용자로 판단
                     # 좌회전
                     veh_state[j*2+1] = left_movement
-            # print(idx, veh_state.sum(), phase_type_tensor.sum())
             # duration 차이의 tensor
             min_dur_tensor = torch.tensor(
-                [self.traffic_node_info[self.tl_rl_list[idx]]['dif_min'][int(action_index_matrix[idx]/2)]], dtype=torch.float, device=self.device).view(-1)
+                self.traffic_node_info[self.tl_rl_list[idx]]['dif_min'][int(action_index_matrix[idx]/2)], dtype=torch.int, device=self.device).view(-1)
             max_dur_tensor = torch.tensor(
-                [self.traffic_node_info[self.tl_rl_list[idx]]['dif_max'][int(action_index_matrix[idx]/2)]], dtype=torch.float, device=self.device).view(-1)
+                self.traffic_node_info[self.tl_rl_list[idx]]['dif_max'][int(action_index_matrix[idx]/2)], dtype=torch.int, device=self.device).view(-1)
             next_state = torch.cat((veh_state, phase_type_tensor, min_dur_tensor, max_dur_tensor), dim=0).view(
                 self.state_space, 1)
             # print(next_state,idx,self.configs['phase_type'][idx])
             # print(next_state)
-            # print((action_index_matrix[idx]/2).long())
+
             self.tl_rl_memory[idx].next_state[:, :, (action_index_matrix[idx]/2).long()
                                               ] = next_state.view(1, self.state_space, 1, 1).detach().clone()
-            # print(self.tl_rl_memory[idx].next_state.view(-1,self.state_space,4))
 
         for idx in torch.nonzero(mask_matrix):
             # next state 생성
-            next_states[0, :, :,idx] = self.tl_rl_memory[idx].next_state.detach().clone()
+            next_states[0, :, :,
+                        idx] = self.tl_rl_memory[idx].next_state.detach().clone()
             self.tl_rl_memory[idx].state[:, :, (action_index_matrix[idx]/2).long()
                                          ] = self.tl_rl_memory[idx].next_state[:, :, (action_index_matrix[idx]/2).long()].detach().clone()
         # reward clear
